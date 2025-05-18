95325f6ce3f2a54c4287525f22c1ff3abf3380c4
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-18 16:21:06 +0900

    Firebase Email Authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -40,6 +41,13 @@ android {
 }
 
 dependencies {
+
+    implementation(platform(libs.firebase.bom))
+
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.runtime.livedata)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
